db1755b3d87138bab9d77f7ecb1aaecfaf5c41d0
jfpiovesa <78732629+jfpiovesa@users.noreply.github.com>
2021-05-03 01:20:24 -0300

    atualização do botão de salvar

--- app/src/main/java/com/example/ListaPersonagem/ui/activities/FormularioPersonagemActivity.java
+++ app/src/main/java/com/example/ListaPersonagem/ui/activities/FormularioPersonagemActivity.java
@@ -2,15 +2,20 @@ package com.example.ListaPersonagem.ui.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.ListaPersonagem.dao.PersonagenDao;
 import com.example.ListaPersonagem.model.Personagen;
 import com.example.auladia11.R;
+import com.github.rtoshiro.util.format.SimpleMaskFormatter;
+import com.github.rtoshiro.util.format.text.MaskTextWatcher;
 
 import static com.example.ListaPersonagem.ui.activities.ConstantesActivities.CHAVE_PERSONAGEN;
 import static com.example.ListaPersonagem.ui.activities.ConstantesActivities.TITULO_APPBAR_EDITA_PERSONAGEN;
@@ -18,17 +23,34 @@ import static com.example.ListaPersonagem.ui.activities.ConstantesActivities.TIT
 
 public class FormularioPersonagemActivity extends AppCompatActivity {
 
-// variaveis para seren editar os texto referente a elas
+    // variaveis para seren editar os texto referente a elas
     private EditText campoNome;
     private EditText campoNacimento;
     private EditText campoAltura;
-// variaveis para salvar  com referente seu nome
+    // variaveis para salvar  com referente seu nome
     private String nome;
     private String nascimento;
     private String altura;
 
     private final PersonagenDao dao = new PersonagenDao();
     private Personagen personagen;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) // criando botão salvar personagem
+    {
+        getMenuInflater().inflate(R.menu.activity_lista_personagens_menu_salvar, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item)// selecionando o botão criado
+    {
+        int itenId = item.getItemId();
+        if (itenId == R.id.Activity_lista_personagens_menu_salvar) {
+            finalizarFormulario();
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState)// metodo responsavel por criar o formulario dos personagens
@@ -61,8 +83,7 @@ public class FormularioPersonagemActivity extends AppCompatActivity {
         campoAltura.setText(personagen.getAltura());
     }
 
-    private void configuraBotaoSalvar()
-    {
+    private void configuraBotaoSalvar() {
         // Pegando  o botao de salvar, para por um listener de ações nele, no caso, salvar  informaçõa do personagem.
         Button btSalvar = findViewById(R.id.bt_salvar);
         btSalvar.setOnClickListener(new View.OnClickListener() {
@@ -74,8 +95,7 @@ public class FormularioPersonagemActivity extends AppCompatActivity {
         );
     }
 
-    private void finalizarFormulario()
-    {
+    private void finalizarFormulario() {
         prencherPersonagen();
         if (personagen.IdValido()) {
             dao.editar(personagen);
@@ -104,5 +124,12 @@ public class FormularioPersonagemActivity extends AppCompatActivity {
         campoNome = findViewById(R.id.editTextText_Name);
         campoNacimento = findViewById(R.id.editText_Nascimento);
         campoAltura = findViewById(R.id.editText_Altura);
+
+        SimpleMaskFormatter smfAltura = new SimpleMaskFormatter("N,NN");
+        MaskTextWatcher mtwAltura = new MaskTextWatcher(campoAltura, smfAltura);
+        campoAltura.addTextChangedListener(mtwAltura);
+        SimpleMaskFormatter smfNascimento = new SimpleMaskFormatter("NN/NN/NNNN");
+        MaskTextWatcher mtwNascimento = new MaskTextWatcher(campoNacimento, smfNascimento);
+        campoNacimento.addTextChangedListener(mtwNascimento);
     }
 }
